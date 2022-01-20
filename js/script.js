4fca915c8ae98e13bf848036d2cb8b02586c095a
Bradley-C-Anderson <97475790+Bradley-C-Anderson@users.noreply.github.com>
2022-01-19 22:54:46 -0600

    Finished for the night

--- js/script.js
+++ js/script.js
@@ -2,8 +2,70 @@
 Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
+document.addEventListener('DOMContentLoaded', () => {
+   const ul = document.querySelector('.student-list');
+   const endNumStudent = data.length;
+   let startNumStudent = 0;
+   const numPerPage = 9;
+   const numPages = Math.ceil(endNumStudent/numPerPage);
 
+   function createElement(elementName, property, value){
+      const element = document.createElement(elementName);
+      element[property] = value;
+      return element;
+   }
+   
+   let currPageNum = 0;
+   showPage(currPageNum);
+   function showPage(pageNum){
+      pageNum = pageNum * numPerPage;
+      for(let i = pageNum; i < pageNum + numPerPage; i++){
+         const li = createElement('li', 'className', 'student-item cf');
+         ul.appendChild(li);
 
+         const div1 = createElement('div', 'className', 'student-details');
+         li.appendChild(div1);
+
+         const img = createElement('img', 'className', 'avatar');
+         img.src = data[i].picture.thumbnail;
+         div1.appendChild(img);
+
+         const name = createElement('h3', 'innerHTML', `${data[i].name.first} ${data[i].name.last}`);
+         div1.appendChild(name);
+
+         const email = createElement('span', 'className', 'email');
+         email.innerHTML = data[i].email;
+         div1.appendChild(email);
+
+         const div2 = createElement('div', 'className', 'joined-details');
+         li.appendChild(div2);
+
+         const joined = createElement('span', 'className', 'date');
+         joined.innerHTML = `Joined ${data[i].registered.date}`;
+         div2.appendChild(joined)
+      }
+}
+
+function removeLastPage(){
+   
+   // for(let i = 0; i < ul.children.length; i++){
+   //    ul.removeChild;
+   // }
+}
+//create page buttons
+   const ulPage = document.querySelector('.pagination');
+   for(let i = 0; i < numPages; i++){
+      const pageButton = createElement('button', 'textContent', `${i+1}`);
+      ulPage.appendChild(pageButton);
+
+      ulPage.addEventListener('click', (e) => {
+         if (e.target.textContent === `${i+1}`){
+            showPage(i);
+         }
+      });
+   }
+
+//ul.removeChild('li');
 
 /*
 For assistance:
@@ -28,3 +90,10 @@ This function will create and insert/append the elements needed for the paginati
 
 
 // Call functions
+
+
+
+
+
+
+});//end of DOMContentLoaded handler
